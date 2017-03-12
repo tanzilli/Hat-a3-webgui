a4d1255f396fbd183ff6a88b1e447703799a2afe
Sergio Tanzilli <tanzilli@acmesystems.it>
2017-03-12 18:29:12 +0000

    Fatta un po di pulizia

--- play.php
+++ play.php
@@ -1,7 +1,7 @@
 <?php
 	if ($_POST['cmd']=="play") {
 		file_put_contents("/var/www/html/slides.env",$_POST['value']);
-		file_put_contents("/var/www/html/slides.txt",$_POST['data']);
+		file_put_contents("/var/www/html/slides.json",$_POST['data']);
 	}
 	
 	if ($_POST['cmd']=="stop") {
