a1153dab24e51faa9c61508a320b943b04bdce0e
Sergio Tanzilli <tanzilli@acmesystems.it>
2017-09-21 16:14:07 +0000

    Player in versione Python

--- play.py
+++ play.py
@@ -1,4 +1,26 @@
 #!/bin/python
 
-rgb-led-matrix-pathname="/home/pi/rpi-rgb-led-matrix/utils/"
+import json
+import time
+import os
 
+images_list="slides.json"
+
+
+while True:
+	with open(images_list) as json_data:
+		d = json.load(json_data)
+	
+		for images_field in d:
+			print(images_field["type"])
+			print(images_field["file"])
+			print("--------------------")
+			
+			command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % images_field["file"]
+			print "  " , command
+			print("--------------------")
+			os.system(command)
+
+	
+	print "Wait..."
+	time.sleep(1)
