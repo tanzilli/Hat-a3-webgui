2df93906cafba0a26e535cd1999740401918a8c6
Sergio Tanzilli <tanzilli@acmesystems.it>
2017-09-21 16:46:19 +0000

    Controllo del blocco del play tra una slide e l'altra

--- play.php
+++ play.php
@@ -1,11 +1,11 @@
 <?php
 	if ($_POST['cmd']=="play") {
-		file_put_contents("/var/www/html/slides.env",$_POST['value']);
 		file_put_contents("/var/www/html/slides.json",$_POST['data']);
+		file_put_contents("/run/ledplay","running");
 	}
 	
 	if ($_POST['cmd']=="stop") {
-		file_put_contents("/var/www/html/slides.env"," ");
+		file_put_contents("/run/ledplay","");
 	}	
 ?>
 
--- play.py
+++ play.py
@@ -3,24 +3,36 @@
 import json
 import time
 import os
+import stat
 
 images_list="slides.json"
+my_runfile = "/run/ledplay"
 
+if not os.path.exists(my_runfile):
+	os.mknod(my_runfile)	
+	os.chmod(my_runfile, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
 
 while True:
-	with open(images_list) as json_data:
-		d = json.load(json_data)
+	if os.path.exists(my_runfile) and os.path.getsize(my_runfile) > 0:
+
+		with open(images_list) as json_data:
+			d = json.load(json_data)
 	
-		for images_field in d:
-			print(images_field["type"])
-			print(images_field["file"])
-			print("--------------------")
+			for images_field in d:
+				print(images_field["type"])
+				print(images_field["file"])
+				print("--------------------")
 			
-			command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % images_field["file"]
-			print "  " , command
-			print("--------------------")
-			os.system(command)
+				command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % images_field["file"]
+				print "  " , command
+				print("--------------------")
+				os.system(command)
+				
+				if os.path.getsize(my_runfile) == 0:
+					break
 
 	
 	print "Wait..."
 	time.sleep(1)
+
+
