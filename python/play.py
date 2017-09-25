c18b5a1620279bcf389564a09e5e980360f79a38
Sergio Tanzilli <tanzilli@acmesystems.it>
2017-09-25 20:40:42 +0200

    Autorun at startup

--- python/play.py
+++ python/play.py
@@ -5,18 +5,27 @@ import time
 import os
 import stat
 
-images_list="/var/www/html/slides.json"
+path, filename = os.path.split(os.path.realpath(__file__))
+
+images_list= "../slides.json"
+slides_dir = "../slides"
 my_runfile = "/run/ledplay"
-slides_path = "/var/www/html/slides/"
 
 if not os.path.exists(my_runfile):
 	os.mknod(my_runfile)	
 	os.chmod(my_runfile, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
 
+f = open(my_runfile,"w")
+f.write("go")
+f.close()
+
+command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % (path + "/" + slides_dir + "/" + "logo.png")
+os.system(command)
+
 while True:
 	if os.path.exists(my_runfile) and os.path.getsize(my_runfile) > 0:
 
-		with open(images_list) as json_data:
+		with open(path + "/" + images_list) as json_data:
 			d = json.load(json_data)
 	
 			for images_field in d:
@@ -26,25 +35,23 @@ while True:
 				print(images_field["file"])
 
 				if images_field["type"]=="ptime":
-					command="/var/www/html/python/ptime.py"
+					command=path + "/" + "ptime.py"
 					os.system(command)
 					command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % ("/tmp/ptime.png")
 					os.system(command)
 					continue
 
 				if images_field["type"]=="ptemp":
-					command="/var/www/html/python/ptemp.py"
+					command=path + "/" + "ptemp.py"
 					os.system(command)
 					command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % ("/tmp/ptemp.png")
 					os.system(command)
 					continue
 
 				if images_field["type"]=="image":
-					command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % (slides_path + images_field["file"])
+					command="/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l1 --led-chain=6 --led-parallel=3 -R270 -w 5 %s" % (path + "/" + slides_dir + "/" + images_field["file"])
 					os.system(command)
 				
-
-	
 	print "Wait..."
 	time.sleep(1)
 
